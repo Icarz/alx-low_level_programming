a80bf8031a99c5a3f64c1f9319ac1b1312266d9f
Icarz <Mr.rghay@gmail.com>
2024-06-25 07:35:33 -0700

    Implement times_table function to print the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+
+			if (product > 9)
+			{
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				if (column > 0)
+					_putchar(' ');
+				_putchar(product + '0');
+			}
+
+			if (column < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-void jack_bauer(void);
+void times_table(void);
 
 #endif /* MAIN_H */
+
 
 
 
