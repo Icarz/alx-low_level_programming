4876be0b8083ee9f8cde93b242ead2e30e8baeb0
Icarz <Mr.rghay@gmail.com>
2024-06-25 07:44:44 -0700

    fixing the table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,21 +15,23 @@ void times_table(void)
 
 			if (product > 9)
 			{
-				_putchar(' ');
+				if (column > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
 			}
 			else
 			{
 				if (column > 0)
+				{
+					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar(product + '0');
-			}
-
-			if (column < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
