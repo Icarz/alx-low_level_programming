7fcced18c6414579e4e286404d38bf03584340be
Icarz <Mr.rghay@gmail.com>
2024-06-26 06:57:48 -0700

    fixing all

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,16 @@
 /* Function prototypes */
 void print_to_98(int n);
 int _putchar(char c);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
 
