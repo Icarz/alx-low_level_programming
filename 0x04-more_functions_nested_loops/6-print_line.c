ae88569f605d50ebd5c59989058c24a9dbbcadfd
Icarz <Mr.rghay@gmail.com>
2024-06-27 14:32:05 -0700

    gotta work

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
-* print_line - Prints a line of underscores followed by a '$' sign
-* @n: Number of underscores to print
-*/
+ * print_line - that draws a straight line in the terminal
+ * @n: the number of times the character _
+ * followed by a new line
+ */
 void print_line(int n)
 {
 int i;
 if (n <= 0)
 {
-_putchar('\n'); /* Print newline if n is 0 or less */
-return;
+_putchar('\n');
 }
+else
+{
 for (i = 0; i < n; i++)
 {
-_putchar('_'); /* Print underscores */
+_putchar('_');
 }
-_putchar('$'); /* Print the '$' sign */
-_putchar('\n'); /* Print newline at the end */
+_putchar('\n');
+}
 }
