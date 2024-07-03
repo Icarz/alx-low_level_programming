cb3bcae9924042ce7d47f484ef38b2cddffca0c8
Icarz <Mr.rghay@gmail.com>
2024-07-03 14:37:04 -0700

    fixing whitespace

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper - Changes all lowercase letters of a string to uppercase.
  * @str: The string to be modified.
- * 
+ *
  * Return: A pointer to the modified string.
  */
 char *string_toupper(char *str)
