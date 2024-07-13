be5bf8b7f4d6901d617532a590db3190f671ae5c
Icarz <Mr.rghay@gmail.com>
2024-07-12 18:48:28 -0700

    night owl

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,47 +2,27 @@
 #include <stdlib.h>
 
 /**
-*main - prints the minimum number of coins to make change
-* for an amount of money
+*main - adds positive numbers
 *@argc: number of arguments
 *@argv: array of arguments
-*Return: returns 1 if there is an error; else returns 0
+*Return: 0 on success, 1 on failure
 */
 
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
-if (argc != 2)
+int i, j, sum = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("Error\n");
 return (1);
 }
-cents = atoi(argv[1]);
-while (cents > 0)
-{
-coins++;
-if ((cents - 25) >= 0)
-{
-cents -= 25;
-continue;
 }
-if ((cents - 10) >= 0)
-{
-cents -= 10;
-continue;
+sum += atoi(argv[i]);
 }
-if ((cents - 5) >= 0)
-{
-cents -= 5;
-continue;
-}
-if ((cents - 2) >= 0)
-{
-cents -= 2;
-continue;
-}
-cents--;
-}
-printf("%d\n", coins);
+printf("%d\n", sum);
 return (0);
 }
