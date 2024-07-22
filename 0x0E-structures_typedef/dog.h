7d64a16c035ecbfc8d7cb1000cbbb282b0cb225e
Icarz <Mr.rghay@gmail.com>
2024-07-22 06:59:04 -0700

    task_3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,9 @@ float age;
 char *owner;
 };
 
+/* Define a new type dog_t as an alias for struct dog */
+typedef struct dog dog_t;
+
 /* Function prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
