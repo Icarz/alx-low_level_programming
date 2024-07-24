4ebd8449ac3e25bb10b5d15fa4dcbfac334bfd93
Icarz <Mr.rghay@gmail.com>
2024-07-24 05:58:19 -0700

    we call it a day

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "calc.h"
+
+/**
+ * op_add - Adds two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The result of the division of a by b.
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - Computes the modulo of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+ return (a % b);
+}
