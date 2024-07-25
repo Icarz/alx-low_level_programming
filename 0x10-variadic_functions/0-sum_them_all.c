2f43947bbd8d360b02c92a4ffe662d7d5be40c6f
Icarz <Mr.rghay@gmail.com>
2024-07-25 05:13:37 -0700

    fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: number of parameters
- * 
+ *
  * Return: sum of all parameters or 0 if n is 0
  */
 int sum_them_all(const unsigned int n, ...)
